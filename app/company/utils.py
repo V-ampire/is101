39485708a91c665be4ee8e2a7d2d1fc800ef0f06
V-ampire <webjob01@mail.ru>
2020-11-04 13:34:48 +1000

    add more fields

---
+++ app/company/utils.py
@@ -0,0 +1,5 @@
+def get_employee_pasport_scan_path(instance, filename):
+    """
+    Возвращает путь к директории в которую сохраняются сканы паспортов.
+    """
+    return f"employees_pasports/{instance.user.first_name}_{instance.user.last_name}"
