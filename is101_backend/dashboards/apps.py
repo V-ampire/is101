704e1b484e73532f463900449d594555007a9886
V-ampire <webjob010@gmail.com>
2020-12-31 12:12:47 +1000

    company viewset

--- is101_backend/dashboards/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class AdminsDashboardConfig(AppConfig):
-    name = 'admins_dashboard'
--- is101_backend/dashboards/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from dashboards import views
-
-
-app_name = 'dashboards'
-urlpatterns = [
-    path('', views.AdminDashboardView.as_view(), name='admin'),
-]
